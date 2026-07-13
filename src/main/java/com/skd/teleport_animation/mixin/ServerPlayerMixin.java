d458468968f15e3a1dbc6afc7defad7b5f66e3e1
LlaGuiTo <santiago.losada.borrajo@gmail.com>
2026-07-13 12:30:43 +0200

    Fix ServerPlayerMixin teleportTo signature (void + boolean resetCamera)

--- src/main/java/com/skd/teleport_animation/mixin/ServerPlayerMixin.java
+++ src/main/java/com/skd/teleport_animation/mixin/ServerPlayerMixin.java
@@ -8,14 +8,14 @@ import net.minecraft.world.entity.Relative;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayer.class)
 public abstract class ServerPlayerMixin {
-    @Inject(method = "teleportTo(Lnet/minecraft/server/level/ServerLevel;DDDLjava/util/Set;FF)Z", at = @At("HEAD"), cancellable = true)
-    private void teleportAnimation$delayExternalTeleport(ServerLevel level, double x, double y, double z, Set<Relative> relatives, float yaw, float pitch, CallbackInfoReturnable<Boolean> cir) {
-        if (TeleportServer.tryDelayExternalTeleport((ServerPlayer) (Object) this, level, x, y, z, relatives, yaw, pitch, false)) {
-            cir.setReturnValue(true);
+    @Inject(method = "teleportTo(Lnet/minecraft/server/level/ServerLevel;DDDLjava/util/Set;FFZ)V", at = @At("HEAD"), cancellable = true)
+    private void teleportAnimation$delayExternalTeleport(ServerLevel level, double x, double y, double z, Set<Relative> relatives, float yaw, float pitch, boolean resetCamera, CallbackInfo ci) {
+        if (TeleportServer.tryDelayExternalTeleport((ServerPlayer) (Object) this, level, x, y, z, relatives, yaw, pitch, resetCamera)) {
+            ci.cancel();
         }
     }
 }
